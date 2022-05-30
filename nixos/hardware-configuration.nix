bf131dac73b694b239cb2ae45a95d5508c216638
IGI-111 <igi-111@protonmail.com>
2022-05-30 04:55:29 +0200

    XPS firmware fixes

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -4,29 +4,40 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "vmd" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.availableKernelModules = [
+    "xhci_pci"
+    "thunderbolt"
+    "vmd"
+    "nvme"
+    "usb_storage"
+    "sd_mod"
+    "rtsx_pci_sdmmc"
+  ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelPackages = lib.mkIf (lib.versionOlder pkgs.linux.version "5.12")
+    (lib.mkDefault pkgs.linuxPackages_latest);
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/b80701fd-d439-4828-afdb-6b2cb91d7313";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/b80701fd-d439-4828-afdb-6b2cb91d7313";
+    fsType = "ext4";
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/1BD7-9833";
-      fsType = "vfat";
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/1BD7-9833";
+    fsType = "vfat";
+  };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/fbf9ca24-cbd7-4964-9318-7f8ef6963fcd"; }
-    ];
+    [{ device = "/dev/disk/by-uuid/fbf9ca24-cbd7-4964-9318-7f8ef6963fcd"; }];
+
+  hardware.enableRedistributableFirmware = true;
+  services.fwupd.enable = true;
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.intel.updateMicrocode =
+    lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
